a1400b8d4f02c44e15c662197bc90306ce638939
KIKI <allairekilian@gmail.com>
2020-05-28 14:22:56 +0200

    Fx PArt 1

---
+++ KUBIKA/Assets/Scripts/_Kilian/_FeedBack/FB_Delivry.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kubika.Game
+{
+    public class FB_Delivry : MonoBehaviour
+    {
+        public ParticleSystem PS;
+        public Transform pivotPS;
+
+        public void ActivatePSFB()
+        {
+            PS.Play();
+        }
+    }
+}
--- KUBIKA/Assets/Scripts/_Kilian/_FeedBack/_FeedBackManager.cs
+++ KUBIKA/Assets/Scripts/_Kilian/_FeedBack/_FeedBackManager.cs
@@ -6,6 +6,17 @@ namespace Kubika.Game
 {
     public class _FeedBackManager : MonoBehaviour
     {
+        public GameObject Fb_Delivry;
+
+        private static _FeedBackManager _instance;
+        public static _FeedBackManager instance { get { return _instance; } }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this) Destroy(this);
+            else _instance = this;
+
+        }
 
         // Start is called before the first frame update
         void Start()
