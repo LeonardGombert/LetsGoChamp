a477b2a09a4fc808daca79dbacb0943f94688c74
Léonard Gombert <epoc4oftime@gmail.com>
2020-06-09 18:07:08 +0200

    Setup Worldmap

--- KUBIKA/Assets/Editor/LevelCubeEditorWindow.cs
+++ KUBIKA/Assets/Editor/LevelCubeEditorWindow.cs
@@ -168,7 +168,7 @@ public class LevelCubeEditorWindow : EditorWindow
         levelCubeObject.transform.position = worldMapFace.transform.GetChild(0).transform.position;
 
         levelCubeObject.transform.up = worldMapFace.transform.GetChild(1).transform.up;
-        levelCubeObject.transform.forward = worldMapFace.transform.GetChild(1).transform.up;
+        //levelCubeObject.transform.forward = worldMapFace.transform.GetChild(1).transform.up;
 
         levelCubeObject.transform.Translate(new Vector3(0, .5f, 0), Space.Self);
         //levelCubeObject.transform.position +=levelCubeObject.transform.Translate(Vector3.up, Space.Self);// levelCubeObject.transform.localPosition + new Vector3(0, .5f, 0);
