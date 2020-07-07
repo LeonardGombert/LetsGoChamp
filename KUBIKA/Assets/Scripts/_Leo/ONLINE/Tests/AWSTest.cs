9e8f38b8d6b8b2e0c704389e03337d2b90c2e156
Léonard <epoc4oftime@gmail.com>
2020-07-07 02:02:26 +0200

    Bon c'est chaud mais déjà j'ai vite f appris à lire, c'est lourd my khey

--- KUBIKA/Assets/Scripts/_Leo/ONLINE/Tests/AWSTest.cs
+++ KUBIKA/Assets/Scripts/_Leo/ONLINE/Tests/AWSTest.cs
@@ -2,10 +2,9 @@
 using Amazon.CognitoIdentity;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,10 +13,14 @@ public class AWSTest : MonoBehaviour
     public int cubeId;
     public Text resultText;
     public string identityPoolId = "eu-central-1:f8990c10-6786-4c01-adc7-6614e9da700b";
-    
+
     CognitoAWSCredentials credentials;
     IAmazonDynamoDB client;
     DynamoDBContext context;
+    IDynamoDBContext _context;
+    DynamoDBOperationConfig _operationConfig;
+
+    public TextAsset myLevelBruther;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +29,8 @@ public class AWSTest : MonoBehaviour
         AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
 
         GetUserCredentials();
-        DescribeTable();
+        DescribeTable(); 
+        SaveLevel();
     }
 
     private void GetUserCredentials()
@@ -36,6 +40,25 @@ public class AWSTest : MonoBehaviour
         context = new DynamoDBContext(client);
 
         Debug.Log("My Received credentials are : " + credentials.AccountId);
+    }
+
+    private void CreateAnObject()
+    {
+        context.SaveAsync<TextAsset>(myLevelBruther, (result) =>
+        {
+            if (result.Exception == null)
+                resultText.text += @"book saved";
+        });
+    }
+
+    public void SaveLevel()
+    {
+        _operationConfig = new DynamoDBOperationConfig
+        {
+            OverrideTableName = "KUBIKA_Testing"
+        };
+
+        context.SaveAsync(Document.FromJson(myLevelBruther.ToString()), _operationConfig, null);
     }
 
     private void DescribeTable()
