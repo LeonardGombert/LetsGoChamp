d6b7cfa2e8eb2fc2a1edaaaa70ab58848f2afff8
Léonard <epoc4oftime@gmail.com>
2020-08-08 21:16:45 +0200

    Deleted Firebase Project. Back to AWS. Working on Cognito Authentication

--- KUBIKA/Assets/Scripts/_Leo/ONLINE/ClientFactory.cs
+++ KUBIKA/Assets/Scripts/_Leo/ONLINE/ClientFactory.cs
@@ -1,24 +1,45 @@
-﻿using Amazon.CognitoIdentity;
+﻿using Amazon;
+using Amazon.Runtime;
 using Amazon.DynamoDBv2;
-using Amazon;
+using Amazon.CognitoIdentity;
+using Amazon.DynamoDBv2.Model;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
 
 namespace Kubika.Online
 {
     public struct AmazonCognito
     {
         public static string identityPoolId = "eu-central-1:f8990c10-6786-4c01-adc7-6614e9da700b";
-        public static RegionEndpoint region = RegionEndpoint.EUCentral1;
+        public static string appClientId = "9db7pkdnppvrlui6sackifg6d";
+        public static string userPoolId = "eu-central-1_UXwaae0M4";
+        public static string userPoolName = "UXwaae0M4";
         public static string authenticatedUserId = "12345";
+        public static RegionEndpoint cognitoIdentityRegion = RegionEndpoint.EUCentral1;
     }
 
     public class ClientFactory
     {
         public static AmazonDynamoDBClient GetUserIdentity()
         {
-            var credentials = new CognitoAWSCredentials(AmazonCognito.identityPoolId, AmazonCognito.region);
-            var client = new AmazonDynamoDBClient(credentials, AmazonCognito.region);
+            var credentials = new CognitoAWSCredentials(AmazonCognito.identityPoolId, AmazonCognito.cognitoIdentityRegion);
+            var client = new AmazonDynamoDBClient(credentials, AmazonCognito.cognitoIdentityRegion);
 
             return client;
+        }
+
+        private static AmazonCognitoIdentityProviderClient _cognitoIdentityProvider = null;
+        public static AmazonCognitoIdentityProviderClient CognitoIdentityProvider 
+        {
+            get {
+                if (_cognitoIdentityProvider == null)
+                {
+                    var config = new AmazonCognitoIdentityProviderConfig();
+                    config.RegionEndpoint = AmazonCognito.cognitoIdentityRegion;
+                    _cognitoIdentityProvider = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(), config);
+                }
+                return _cognitoIdentityProvider;
+            }
         }
     }
 }
---
+++ KUBIKA/Assets/Scripts/_Leo/ONLINE/DynamoDB/ClientHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Amazon.CognitoIdentityProvider.Model;
+using Kubika.Online;
+using Amazon.CodeCommit.Model;
+using Amazon.Lambda.Model;
+
+public class ClientHandler : MonoBehaviour
+{
+    private static ClientHandler _instance;
+    public static ClientHandler instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this) Destroy(this);
+        else _instance = this;
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // signup method, called by SignUp button Event in UIManager
+    void TrySignUpRequest(string email, string password, Action OnFailure = null, Action OnSuccess = null)
+    {
+        SignUpRequest signUpRequest = new SignUpRequest
+        {
+            ClientId = AmazonCognito.appClientId,
+            Username = email,
+            Password = password
+        };
+
+        var emailAttribute = new AttributeType
+        {
+            Name = "email",
+            Value = email
+        };
+
+        signUpRequest.UserAttributes.Add(emailAttribute);
+
+        var response = ClientFactory.CognitoIdentityProvider.SignUpAsync(signUpRequest);
+
+        if(response.Exception != null)
+        {
+            Debug.Log("Failed to complete signup, returned error : \n " + response.Exception.ToString());
+            OnFailure();
+        }
+
+        else
+        {
+            Debug.Log("Signup Complete");
+            OnSuccess();
+        }
+    }
+}
