95aab2854ed55c7dfe71ba2f7bcb39b6e4a69134
Léonard <epoc4oftime@gmail.com>
2020-09-03 16:22:48 +0200

    Swipe Between worlds

--- KUBIKA/Assets/Scripts/_Leo/WorldmapLevelPopulator.cs
+++ KUBIKA/Assets/Scripts/_Leo/WorldmapLevelPopulator.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 public class WorldmapLevelPopulator : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     [SerializeField] GameObject levelPrefab;
+    Transform LevelListTransfrom;
     LevelsManager levelsManager;
     public float percentThreshold = 0.2f;
     Vector3 panelLocation;
@@ -14,15 +15,16 @@ public class WorldmapLevelPopulator : MonoBehaviour, IDragHandler, IEndDragHandl
     // Start is called before the first frame update
     void Start()
     {
+        LevelListTransfrom = transform.GetChild(0).GetChild(0).GetChild(0);
         levelsManager = LevelsManager.instance;
-        PopulateScreen();
+        PopulateScreen(1);
         panelLocation = transform.position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        float difference = eventData.pressPosition.x - eventData.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
+        //float difference = eventData.pressPosition.x - eventData.position.x;
+        //transform.position = panelLocation - new Vector3(difference, 0, 0);
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -30,47 +32,47 @@ public class WorldmapLevelPopulator : MonoBehaviour, IDragHandler, IEndDragHandl
         float percentage = (eventData.pressPosition.x - eventData.position.x) / Screen.width;
         if (Mathf.Abs(percentage) >= percentThreshold)
         {
-            Vector3 newLocation = panelLocation;
-            if (percentage > 0)
+            foreach (Transform child in LevelListTransfrom) Destroy(child.gameObject);
+
+            //Vector3 newLocation = panelLocation;
+            if (percentage > 0 && currentWorld < (int)Biomes.Count)
             {
-                newLocation += new Vector3(-Screen.width, 0, 0);
-                currentWorld--;
-            }
-            else if (percentage < 0)
-            {
-                newLocation += new Vector3(Screen.width, 0, 0);
                 currentWorld++;
             }
 
-            transform.position = newLocation;
-            panelLocation = newLocation;
+            else if (percentage < 0 && currentWorld > 1)
+            {
+                currentWorld--;
+            }
+
+            //transform.position = newLocation;
+            //panelLocation = newLocation;
             Debug.Log("Swiped to next page");
             Debug.Log("Current World is " + currentWorld);
+            
+            PopulateScreen(currentWorld);
         }
         else
         {
-            transform.position = panelLocation;
+            //transform.position = panelLocation;
             Debug.Log("Failed swipe");
         }
     }
 
     // populate the list with the levels in the current world
-    void PopulateScreen()
+    void PopulateScreen(int _currentWorld)
     {
-        for (int _currentWorld = 1; _currentWorld <= 6; _currentWorld++)
+        foreach (var item in levelsManager.gameMasterList)
         {
-            foreach (var item in levelsManager.gameMasterList)
+            if (item.kubicode.Contains("Worl" + _currentWorld))
             {
-                if (item.kubicode.Contains("Worl" + _currentWorld))
-                {
-                    GameObject newObj = Instantiate(levelPrefab, Vector3.zero, Quaternion.identity, transform.GetChild(_currentWorld-1).GetChild(0).GetChild(0));
-                    LevelInstantiator level = newObj.GetComponent<LevelInstantiator>();
-                    level.FillValues(item.levelName, item.difficulty, item.kubicode);
-                }
-
-                // if you're reading items of the next world, stop the foreach loop to save on time
-                else if (item.kubicode.Contains("World" + (_currentWorld + 1))) break;
+                GameObject newObj = Instantiate(levelPrefab, Vector3.zero, Quaternion.identity, LevelListTransfrom);
+                LevelInstantiator level = newObj.GetComponent<LevelInstantiator>();
+                level.FillValues(item.levelName, item.difficulty, item.kubicode);
             }
+
+            // if you're reading items of the next world, stop the foreach loop to save on time
+            else if (item.kubicode.Contains("World" + (_currentWorld + 1))) break;
         }
     }
 
